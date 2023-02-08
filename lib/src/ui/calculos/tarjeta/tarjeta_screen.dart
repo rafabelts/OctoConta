1d2f0fef28b3f949ef02dd08a04ec767e19451ff
rafabelts <rafabeltrans17@gmail.com>
2023-02-07 22:48:58 -0600

    Funcionalidad prima, prestamo e IVA

--- lib/src/ui/calculos/tarjeta/tarjeta_screen.dart
+++ lib/src/ui/calculos/tarjeta/tarjeta_screen.dart
@@ -3,8 +3,65 @@ import 'package:octoconta_final/src/models/buttons_calculos.dart';
 import 'package:octoconta_final/src/models/muestra_resultados.dart';
 import 'package:octoconta_final/src/ui/calculos/tarjeta/tarjeta_inputs.dart';
 
-class CalculoTarjetaScreen extends StatelessWidget {
+class CalculoTarjetaScreen extends StatefulWidget {
   const CalculoTarjetaScreen({super.key});
+
+  @override
+  State<CalculoTarjetaScreen> createState() => _CalculoTarjetaScreenState();
+}
+
+class _CalculoTarjetaScreenState extends State<CalculoTarjetaScreen> {
+  TextEditingController deuda = TextEditingController();
+  TextEditingController interes = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    deuda;
+    interes;
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    deuda.dispose();
+    interes.dispose();
+  }
+
+  bool esNumeroDeuda = true;
+  bool esNumeroInteres = true;
+
+  setValidador(valid, valid1) {
+    setState(() {
+      esNumeroDeuda = valid;
+    });
+  }
+
+  onChanged() {
+    if (deuda.text.isEmpty) {
+      esNumeroDeuda = false;
+    } else {
+      esNumeroDeuda = true;
+    }
+  }
+
+  onComplete() {
+    if (deuda.text.isEmpty) {
+      esNumeroDeuda = false;
+    } else {
+      esNumeroDeuda = true;
+      FocusScope.of(context).nextFocus();
+    }
+  }
+
+  onSubmitted() {
+    FocusScope.of(context).unfocus();
+    if (interes.text.isEmpty) {
+      esNumeroInteres = false;
+    } else {
+      esNumeroInteres = true;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +71,7 @@ class CalculoTarjetaScreen extends StatelessWidget {
         padding: const EdgeInsets.symmetric(vertical: 30.0),
         child: Column(
           children: <Widget>[
-            const CalculoTarjetaInput(),
+            ResultadosTarjetaItems(),
             Botones(
               limpiar: () {},
               calcular: () => mostrarResultados(
